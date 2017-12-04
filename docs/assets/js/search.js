c1e612553a271c340aa43263bdc6582c643adbf0
thegecko <github@thegecko.org>
2017-12-04 00:53:04 +0000

    Automatic Deployment [skip ci]

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+﻿var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"128":"Class"},"rows":[{"id":0,"kind":128,"name":"WebUSB","url":"classes/webusb.html","classes":"tsd-kind-class"}]};
